7db23b8594a68375148a1ddfc0f1d0e8cc0be1b6
FSorek <filip.sorek96@gmail.com>
2022-12-12 17:59:52 +0100

    Added missing files to git, completed day 1 part 2

---
+++ Day01/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+List<int> caloriesSums = new List<int>();
+int currentCalories = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    string line = input[i];
+    if(string.IsNullOrEmpty(line))
+    {
+        caloriesSums.Add(currentCalories);
+        currentCalories = 0;
+    }
+    else
+    {
+        if (int.TryParse(line, out int calories))
+            currentCalories += calories;
+    }
+}
+Console.WriteLine($"Top calories: {caloriesSums.OrderBy(cal => cal).Last()}");
+Console.WriteLine($"Top 3 calories: {caloriesSums.OrderBy(cal => cal).TakeLast(3).Sum()}");
