82d7002010c192b9354aa19c69a377a4c6a577ad
FSorek <filip.sorek96@gmail.com>
2022-12-12 20:02:59 +0100

    Completed day 2 part 1

---
+++ Day02/Program.cs
@@ -0,0 +1,34 @@
+﻿using Day02;
+
+Dictionary<string, Shape> moves = new Dictionary<string, Shape>()
+{
+    {"A", Shape.Rock},
+    {"B", Shape.Paper},
+    {"C", Shape.Scissors},
+    {"X", Shape.Rock},
+    {"Y", Shape.Paper},
+    {"Z", Shape.Scissors},
+};
+string[] input = File.ReadAllLines("input.txt");
+int myScore = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    string line = input[i];
+    string[] currentPlay = line.Split(" ");
+    myScore += GetScore(currentPlay[0], currentPlay[1]);
+}
+Console.WriteLine(myScore);
+int GetScore(string enemyMove, string myMove)
+{
+    var myShape = moves[myMove];
+    var enemyShape = moves[enemyMove];
+    
+    if (myShape == enemyShape)
+        return 3 + myShape.BaseScore;
+    else
+    {
+        if (myShape.Weakness == enemyShape.Name)
+            return 0 + myShape.BaseScore;
+        else return 6 + myShape.BaseScore;
+    }
+}
---
+++ Day02/Shape.cs
@@ -0,0 +1,33 @@
+﻿namespace Day02;
+
+public class Shape
+{
+    public static Shape Rock = new Shape()
+    {
+        Name = ShapeNames.Rock,
+        Weakness = ShapeNames.Paper,
+        BaseScore = 1,
+    };
+    public static Shape Paper = new Shape()
+    {
+        Name = ShapeNames.Paper,
+        Weakness = ShapeNames.Scissors,
+        BaseScore = 2,
+    };
+    public static Shape Scissors = new Shape()
+    {
+        Name = ShapeNames.Scissors,
+        Weakness = ShapeNames.Rock,
+        BaseScore = 3,
+    };
+    public ShapeNames Name { get; init; }
+    public ShapeNames Weakness { get; init; }
+    public int BaseScore { get; init; }
+}
+
+public enum ShapeNames
+{
+    Rock,
+    Paper,
+    Scissors
+}
